41833a3cf1f4440266e38887aeb600bb08120417
Sankir52 <lapkinav@gmail.com>
2021-12-17 11:08:14 +0300

    Инициализация маркеров для выбра теста

--- test_main_page.py
+++ test_main_page.py
@@ -4,24 +4,25 @@
 # 2. проверка доступности ссылки авторизации.
 # 3. Проверка корректности перехода на страницу авторизации и поиск нужных форм.
 # Тест №3 реализован с assert, падение теста на первой же неудачной проверке.
-
+import pytest
 from .pages.base_page import BasePage
 from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self,browser):
+        #link = "http://selenium1py.pythonanywhere.com/"
+        page = BasePage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                      # открываем страницу
+        page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-def A_test_guest_can_go_to_login_page(browser):
-    #link = "http://selenium1py.pythonanywhere.com/"
-    page = BasePage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-
-def A_test_guest_should_see_login_link(browser):
-    #link = "http://selenium1py.pythonanywhere.com/"
-    page = BasePage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self,browser):
+        #link = "http://selenium1py.pythonanywhere.com/"
+        page = BasePage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 def A_test_guest_should_see_login_page(browser):
     #link = "http://selenium1py.pythonanywhere.com/"
